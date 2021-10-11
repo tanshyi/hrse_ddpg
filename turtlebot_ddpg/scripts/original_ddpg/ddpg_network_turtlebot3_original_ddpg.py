d1e38aeea28d00752e629649ae2194038acfb6ec
TS, Yin <tianshi.yin@gmail.com>
2021-10-11 13:18:58 +0800

    Train and test

--- turtlebot_ddpg/scripts/original_ddpg/ddpg_network_turtlebot3_original_ddpg.py
+++ turtlebot_ddpg/scripts/original_ddpg/ddpg_network_turtlebot3_original_ddpg.py
@@ -256,7 +256,7 @@ def main():
 	########################################################
 	num_trials = 10000
 	trial_len  = 500
-	train_indicator = 0
+	train_indicator = 1
 
 	current_state = game_state.reset()
 
@@ -341,8 +341,8 @@ def main():
 			
 			# Minimal model to reach target: model-30-500.h5
 			# Optimal models: model-50-500.h5
-			actor_critic.actor_model.load_weights("/home/nicho/catkin_ws/src/hrse_ddpg/turtlebot_ddpg/scripts/original_ddpg/actormodel-50-500.h5")
-			actor_critic.critic_model.load_weights("/home/nicho/catkin_ws/src/hrse_ddpg/turtlebot_ddpg/scripts/original_ddpg/criticmodel-50-500.h5")
+			actor_critic.actor_model.load_weights("/home/ts/catkin_ws/src/hrse_ddpg/turtlebot_ddpg/scripts/original_ddpg/actormodel-50-500.h5")
+			actor_critic.critic_model.load_weights("/home/ts/catkin_ws/src/hrse_ddpg/turtlebot_ddpg/scripts/original_ddpg/criticmodel-50-500.h5")
 			print("models loaded")
 
 			#actor_critic.actor_model.load_weights("actormodel-160-500.h5")
